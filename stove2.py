5cbaab9adb8b8e91ce0819239cc78ba92d1deecd
rakybond007 <rakybond007@gmail.com>
2020-11-28 22:23:54 +0900

    roster update with position

--- stove2.py
+++ stove2.py
@@ -6,12 +6,13 @@ import time
 pro_status_dict = {}
 web_address = "https://lol.gamepedia.com/Roster_Swaps"
 countries = ["Korea"]
-seasons = ["Current", "2020_Midseason", "2020_Preseason", "2019_Midseason", "2019_Preseason"]
+seasons = ["Current", "2020_Midseason", "2020_Preseason", "2019_Midseason", "2019_Preseason", "2018_Midseason", "2018_Preseason"]
 roster_dict = {}
 progamer_set = set()
 bar_down = 0
 
 def crawl_roster():
+    position_names = ["Top", "Jungle", "Mid", "Bot", "Support", "Head_Coach"]
     for season in seasons:
         if not season in roster_dict:
             roster_dict[season] = {}
@@ -25,15 +26,76 @@ def crawl_roster():
             team_num = len(table)
             for i in range(team_num):
                 #print(roster_dict)
+                if i >= 10:
+                    break
+                table_info = table[i].select("tbody")[0].select("tr")
+                if table[i].select("tbody")[0].select("tr.empty-width-row"):
+                    roster_start_idx = 3
+                    team_name_tr = table_info[1]
+                else:
+                    roster_start_idx = 2
+                    team_name_tr = table_info[0]
+                #team name no change
+                print(season)
+                if not team_name_tr.select("th.rosterswap-current-new"):
+                    team_name = team_name_tr.select("th")[1].find('a').attrs['data-to-id']
+                    #print(team_name)
+                else:
+                    team_name_prev = team_name_tr.select("th.rosterswap-current-old")[0].find('a').attrs['data-to-id']
+                    team_name_post = team_name_tr.select("th.rosterswap-current-new")[0].find('a').attrs['data-to-id']
+                    team_name = team_name_prev + ',' + team_name_post
+                table_roster = table_info[roster_start_idx:]
+                '''
                 progamers_all_table = table[i].select("td.rosterswap-current-old")
                 progamers_join_table = table[i].select("td.rosterswap-current-new.rosterswap-current-join")
                 progamers_leave_table = table[i].select("td.rosterswap-current-old.rosterswap-current-leave")
+                print(len(table[i].select("td.rosterswap-current-rolename")))
+                '''
+                rolenames = table[i].select("td.rosterswap-current-rolename")
+                position_nums = []
+                for role in rolenames:
+                    position_nums.append(int(role.attrs['rowspan']))
+                if not position_nums:
+                    position_nums.append(table[i].select("tr.rosterswap-current-t.rosterswap-current-firstline")[0].select("td")[0].attrs['rowspan'])
+                    position_nums.append(table[i].select("tr.rosterswap-current-j.rosterswap-current-firstline")[0].select("td")[0].attrs['rowspan'])
+                    position_nums.append(table[i].select("tr.rosterswap-current-m.rosterswap-current-firstline")[0].select("td")[0].attrs['rowspan'])
+                    position_nums.append(table[i].select("tr.rosterswap-current-a.rosterswap-current-firstline")[0].select("td")[0].attrs['rowspan'])
+                    position_nums.append(table[i].select("tr.rosterswap-current-s.rosterswap-current-firstline")[0].select("td")[0].attrs['rowspan'])
+                #exit(1)
                 #progamers_join_table = table[i].select("td.rosterswap-current-old.rosterswap-current-join")
-                if table[i].find('th').find('a') == None:
-                    team_name = progamers_all_table[0].find('a').attrs['data-to-id']
-                else:
-                    team_name = table[i].find('th').find('a').attrs['data-to-id']
-                # 2020 mid season 설해원 같은 경우, 팀 로고 칸이 이상하게 구성되어 있고 선수 이름 있는 칸이랑 같은 종류의 태그에 팀이름 적혀있어서 처리
+                position_idx = 0
+                line_per_num = 0
+                roster_dict[season][country][team_name] = {}
+                print(position_nums)
+                for each_player in table_roster:
+                    if each_player.find('a') == None:
+                        continue
+                    progamer_id = each_player.find('a').attrs['data-to-id']
+                    if len(progamer_id.split('_')) > 1:
+                        progamer_id = progamer_id.split('_')[0]
+                    progamer_set.add(progamer_id)
+                    if line_per_num == position_nums[position_idx]:
+                        position_idx += 1
+                        line_per_num = 0
+                    if line_per_num == 0:
+                        roster_dict[season][country][team_name][position_names[position_idx]] = {}
+                    #print(len(each_player.select("td")))
+                    # join 이 없는 라인. 떠나기만 했거나, 남아있거나.
+                    if not each_player.select("td.rosterswap-current-old.rosterswap-current-join"):
+                        # 떠난 사람
+                        if each_player.select("td.rosterswap-current-old.rosterswap-current-leave"):
+                            roster_dict[season][country][team_name][position_names[position_idx]][progamer_id] = "leave"
+                        else:
+                            roster_dict[season][country][team_name][position_names[position_idx]][progamer_id] = "origin"
+                    # join 이 있는 라인. 전임자가 떠남.
+                    else:
+                        if each_player.select("td.rosterswap-current-old.rosterswap-current-leave"):
+                            roster_dict[season][country][team_name][position_names[position_idx]][progamer_id] = "leave"
+                        else:
+                            roster_dict[season][country][team_name][position_names[position_idx]][progamer_id] = "join"
+                    line_per_num += 1
+                # Past version code
+                '''
                 if not team_name in roster_dict:
                     roster_dict[season][country][team_name] = {}
                 for progamer in progamers_all_table:
@@ -58,6 +120,7 @@ def crawl_roster():
                     #print(progamer_id)
                     progamer_set.add(progamer_id)
                     roster_dict[season][country][team_name][progamer_id] = "join"
+                '''
 
 def crawl_progamer_status(pro_id):
     web_address = "https://lol.gamepedia.com/"
@@ -90,7 +153,9 @@ def crawl_progamer_status(pro_id):
             col_names = []
             for col_names_element in col_names_elements:
                 col_names.append(col_names_element.text)
-            
+            col_names[1] = "Games"
+            print(col_names)
+            exit(1)
             #crawl table body
             body_elements_by_champion = table_body.find_elements_by_tag_name("tr")
             for champion_info in body_elements_by_champion:
@@ -105,12 +170,14 @@ def crawl_progamer_status(pro_id):
 #exit(1)
 crawl_roster()
 print(sorted(progamer_set))
-#crawl_progamer_status("Chovy")
+'''
+crawl_progamer_status("Chovy")
 for pro in sorted(progamer_set):
     print(pro)
     crawl_progamer_status(pro)
 with open('pro_status2.json', 'w', encoding='utf-8-sig') as pro_status_file:
     json.dump(pro_status_dict, pro_status_file, indent="\t", ensure_ascii=False)
+'''
 print(roster_dict)
 with open('roster.json', 'w', encoding='utf-8') as roster_file:
     json.dump(roster_dict, roster_file, indent="\t")
